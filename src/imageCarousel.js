287e1e14274dbae8c91570838f5833e4b6e20cf0
TIWIMIN <tiwimin@gmail.com>
2025-01-23 23:45:40 -0800

    Create Image Carousel class

---
+++ src/imageCarousel.js
@@ -0,0 +1,10 @@
+export class ImageCarousel {
+    #carouselContainer = document.querySelector("div.carousel-container"); 
+    #imageContainer = document.querySelector("div.image-container"); 
+    #previousImage = document.querySelector("button.previous-image"); 
+    #nextImage = document.querySelector("button.next-image"); 
+
+    constructor() {
+        
+    }
+}
